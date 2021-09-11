1bc0a89ff6798de7e839aae2c0d96bd57861210e
Jonathan Ramos <jon.ramos995@gmail.com>
2021-09-11 14:38:55 -0400

    Jonathan - Updated some scripts and was able to get Camera effects working.

--- MoonBaseProject/Assets/Scripts/PlayerStatus.cs
+++ MoonBaseProject/Assets/Scripts/PlayerStatus.cs
@@ -1,9 +1,11 @@
 using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
 
 public class PlayerStatus : MonoBehaviour
 {
     public SC_FPSController controller;
     public Camera main;
+    public PostProcessVolume volume;
     
     [Range(0.0f, 10.0f)]
     public float sustenance = 10.0f;
@@ -14,6 +16,11 @@ public class PlayerStatus : MonoBehaviour
     [Range(0.0f, 10.0f)]
     public float sanity = 10.0f;
 
+
+    private DepthOfField dof;
+    private Grain grain;
+    private ChromaticAberration abberation;
+    
     // Start is called before the first frame update
     private void Start()
     {
@@ -21,6 +28,45 @@ public class PlayerStatus : MonoBehaviour
         sustenance = 10.0f;
         stamina = 10.0f;
         sanity = 10.0f;
+
+        dof = volume.sharedProfile.GetSetting<DepthOfField>();
+        grain = volume.sharedProfile.GetSetting<Grain>();
+        abberation = volume.sharedProfile.GetSetting<ChromaticAberration>();
+        
+        
+        // Initialize their default values, cause these dang things don't save etween sessions.
+        if (dof != null)
+        {
+            dof.focusDistance.value = 10.0f;
+            dof.aperture.value = 10.0f;
+            dof.focalLength.value = 100.0f;
+        }
+        else
+        {
+            Debug.LogError("Depth of Field is Missing!");
+        }
+
+        if (grain != null)
+        {
+            grain.colored.value = true;
+            grain.intensity.value = 0.0f;
+            grain.size.value = 0.3f;
+            grain.lumContrib.value = 0.0f;
+        }
+        else
+        {
+            Debug.LogError("Grain is Missing!");
+        }
+
+        if (abberation != null)
+        {
+            abberation.intensity.value = 0.0f;
+            abberation.fastMode.value = false;
+        }
+        else
+        {
+            Debug.LogError("Chromatic Aberration is Missing!");
+        }
     }
 
     // Update is called once per frame
