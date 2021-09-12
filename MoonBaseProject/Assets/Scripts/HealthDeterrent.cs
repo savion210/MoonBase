4a22d4576d655d2b39ef804d9644a72863aa740c
Jonathan Ramos <jon.ramos995@gmail.com>
2021-09-12 09:14:43 -0400

    Removed redundant Using statements.

--- MoonBaseProject/Assets/Scripts/HealthDeterrent.cs
+++ MoonBaseProject/Assets/Scripts/HealthDeterrent.cs
@@ -2,17 +2,13 @@ using UnityEngine;
 
 public class HealthDeterrent : MonoBehaviour
 {
-    
-
     [Range(0.0f, 5.0f)]
     public float staminaHit;
     
     public bool Interaction(PlayerStatus controller)
     {
         if (controller.stamina < staminaHit) return false;
-        
         controller.stamina -= staminaHit;
-
         return true;
     }
 }
